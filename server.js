984eebac28c6338cfd9dcab917a24c1b6eeb9cbf
TahaNada01 <117838958+TahaNada01@users.noreply.github.com>
2024-11-22 18:43:45 +0100

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ import fetch from "node-fetch";
 import cors from "cors";
 
 const app = express();
-const port = process.env.PORT || 8081;
+const port = process.env.PORT || 3001;
 
 // Clé API incluse directement (moins sécurisé)
 const PERENNIAL_API_KEY = "sk-YYcB6740bb468ff2a7737";
